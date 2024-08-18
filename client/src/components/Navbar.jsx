327ac33d777a3bc25e506511aeae0647ad48619f
Akshay120302 <akshay.sinha335@gmail.com>
2024-08-18 18:40:54 +0530

    Fixed css

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -88,10 +88,12 @@ const Navbar = () => {
                     </>
                   ) : (
                     <Link
-                      className="h-fit cursor-pointer rounded-full bg-black px-5 py-1.5 !text-white ml-[-6px]"
                       to="./signin"
                     >
-                      Sign In
+                      <div className="h-[35px] w-[93px] cursor-pointer rounded-full bg-black px-5 py-1.5 !text-white ml-[-6px]">
+                        Sign In
+                      </div>
+                      
                     </Link>
                   )}
                 </div>
